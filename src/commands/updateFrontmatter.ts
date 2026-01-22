a7a4a62a0381acba279c93973f20a5b00e4211cf
manastria <manastria@users.noreply.github.com>
2026-01-22 21:17:17 +0100

    Refactor updateFrontmatter function to initialize permalink as a string instead of an array

--- src/commands/updateFrontmatter.ts
+++ src/commands/updateFrontmatter.ts
@@ -77,9 +77,8 @@ function updateFrontmatter(frontmatter: any, file: TFile): any {
 	frontmatter.level = frontmatter.level || [];
 	frontmatter.option = frontmatter.option || [];
 	frontmatter.target = frontmatter.target || [];
-	frontmatter.slug = frontmatter.slug || [];
 	frontmatter.keywords = frontmatter.keywords || [];
-	frontmatter.permalink = frontmatter.permalink || [];
+	frontmatter.permalink = frontmatter.permalink || "";
 
     return frontmatter;
 }
